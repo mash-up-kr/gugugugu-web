79efffc7dd08212b4fc1752fcc900bc37cb839a5
YukJiSoo <wltn3231@naver.com>
2020-07-25 01:30:46 +0900

    chore: add eslint, prettier
    reference: https://github.com/vercel/next.js/tree/canary/examples/with-typescript-eslint-jest

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import { GetStaticProps } from "next";
+import { GetStaticProps } from 'next';
 
 const Home = () => {
   return <div>gugugugu</div>;
 };
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps = async () => {
   return { props: {} };
 };
 
