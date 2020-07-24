ef78380b81d9a12f53db4ff46eb6f3dc2e16a990
YukJiSoo <wltn3231@naver.com>
2020-07-25 01:20:23 +0900

    chore: create next app with typescript

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { GetStaticProps } from "next";
+
+const Home = () => {
+  return <div>gugugugu</div>;
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  return { props: {} };
+};
+
+export default Home;
