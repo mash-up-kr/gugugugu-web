8a7b2bd0075ca6fa2d99f67b54e3b5ae1ddd83c8
YukJiSoo <wltn3231@naver.com>
2020-07-26 07:08:51 +0900

    feat(markup): NotYet 페이지 마크업

--- src/pages/modal.tsx
+++ src/pages/modal.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import WriteMailHeader from "~/components/WriteMailHeader";
+import WriteMailContent from "~/components/WriteMailContent";
 
 const WriteMailModal: React.FC = () => {
   return (
     <>
       <WriteMailHeader />
+      <WriteMailContent />
     </>
   );
 };
