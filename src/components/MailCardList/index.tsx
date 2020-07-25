82907b25ef4ca077c11bb65ee41c02547a697a53
YukJiSoo <wltn3231@naver.com>
2020-07-26 05:40:24 +0900

    feat(markup): MailCardList 제목 마크업

--- src/components/MailCardList/index.tsx
+++ src/components/MailCardList/index.tsx
@@ -7,15 +7,24 @@ const MailCardList: React.FC = () => {
   const mockData = new Array(10).fill(10);
 
   return (
-    <CardList>
-      {mockData.map((_, index) => (
-        <li key={index}>
-          <MailCardItem />
-        </li>
-      ))}
-    </CardList>
+    <>
+      <Description>새로운 편지</Description>
+      <CardList>
+        {mockData.map((_, index) => (
+          <li key={index}>
+            <MailCardItem />
+          </li>
+        ))}
+      </CardList>
+    </>
   );
 };
+
+const Description = styled.div`
+  margin: 0 0 20px 20px;
+
+  font-size: 16px;
+`;
 
 const CardList = styled.ul`
   display: flex;
