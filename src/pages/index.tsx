77c49dd2a281a648520102e7ac7a97a46364d997
YukJiSoo <wltn3231@naver.com>
2020-07-26 02:54:26 +0900

    refactor: move main page url to / from /main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,7 @@
-import { GetStaticProps } from "next";
-import styled from "styled-components";
+import BottomTabBar from "~/components/BottomTabBar";
 
-import Hello from "~/components/Hello";
-
-const Home = () => {
-  return (
-    <StyledWrapper>
-      gugugugu
-      <Hello />
-    </StyledWrapper>
-  );
+const Main = () => {
+  return <BottomTabBar />;
 };
 
-const StyledWrapper = styled.div`
-  color: red;
-`;
-
-export const getStaticProps: GetStaticProps = async () => {
-  return { props: {} };
-};
-
-export default Home;
+export default Main;
