8db6f68f998af0f5fdae63f0b9ac44779d82538e
YukJiSoo <wltn3231@naver.com>
2020-07-25 23:09:01 +0900

    chore(style): add global style component

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: auto;
+    max-width: 520px;
+    overflow-x: hidden;
+    min-height: 100%;
+  }
+`;
+
+export default GlobalStyle;
