2f436f159fb6b12ca5b891a6683d060314f41906
YukJiSoo <wltn3231@naver.com>
2020-07-26 06:46:27 +0900

    feat(markup): WriteMailHeader 마크업

---
+++ src/pages/modal.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import WriteMailHeader from "~/components/WriteMailHeader";
+
+const WriteMailModal: React.FC = () => {
+  return (
+    <>
+      <WriteMailHeader />
+    </>
+  );
+};
+
+export default WriteMailModal;
