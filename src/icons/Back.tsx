51fa1190bd524e83208806b9186dda124c754904
YukJiSoo <wltn3231@naver.com>
2020-09-23 22:07:17 +0900

    refactor(icon): 아이콘 컴포넌트 분리

---
+++ src/icons/Back.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Back: React.FC = () => {
+  return (
+    <svg
+      width="11"
+      height="20"
+      viewBox="0 0 11 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <rect width="11" height="20" fill="url(#pattern0)" />
+      <defs>
+        <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
+          <use xlinkHref="#image0" transform="scale(0.0909091 0.05)" />
+        </pattern>
+        <image
+          id="image0"
+          width="11"
+          height="20"
+          xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAsAAAAUCAYAAABbLMdoAAAA30lEQVQ4EY3RvyvFURjA4XNvfkaSUrolJUUGWWSRv89gMljuYFIWZbklEoMy2A0yMFCSRDJYHh3e8Z7vccbzPp8zvCelysEUTrHYSDGBa3+nV8QYw2XAG0z3xRjBWcA7dEpwCL2AD5grwQEcBnzCQgm2sR/wBcsl2EI34BtW+8J8iZ2AH1hvglsBv7BZhCmldkrpO0ArpTTYhH9n2I7XP7Hxn2A3gnesNQbIG9mL4BUrtSDv+iCCZyzVgvyLRxE8Yr4WDOM4gnvM1oJRXERwi5laMI6rCM4bcR5iEid5Oz+cxC8HleqBEAAAAABJRU5ErkJggg=="
+        />
+      </defs>
+    </svg>
+  );
+};
+
+export default Back;
